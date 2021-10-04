fc017d2c26be4616000a6e08dba63c593f90894d
Rafael Oliveira <rafaelfmo@alu.ufc.br>
2021-10-04 18:14:45 -0300

    Estressados 1

--- Estressados.cpp
+++ Estressados.cpp
@@ -5,50 +5,59 @@
 
 bool existe (vector<int> vect, int valor) 
 {
-    for (int i = 0; i < vect.size(); i++){
-        if (vect[i] == valor){
-        return true;}
+    for (size_t i = 0; i < vect.size(); i++)
+    {
+        if (vect[i] == valor)
+        {
+            return true;
+        }
     }    
      return false;
 }
 
-int contar (vector<int> vect, int valor)
+void contar (int valor)
 {
+    vector<int> vect; 
     int contar = 0;
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if (vect[i] == valor) contar++;
     }
-    return contar;   
+     cout << contar; 
 }
 
-int procurar (vector<int> vect, int valor)
+int procurar_valor (int valor)
 {
-    int position = -1;
+    vector<int> vect;
     
-    for (int i = 0; i < vect.size(); i++){
-        position++;
-        if (vect[i] == valor) 
+    for (size_t i = 0; i < vect.size(); i++)
+    {
+        if (vect[i] == valor)
+        return i; 
         break; 
     }
-    return position; 
+    return 0; 
 }
  
-int procurar2 (vector<int> vect, int valor, int posicao)
+int procurar_valor_apos (int valor, int posicao)
 {
-    posicao = 0;
-    for (int i = posicao; i < vect.size(); i++)
+    vector<int> vect;
+    int contar = 0;
+
+    for (size_t posicao; posicao < vect.size(); posicao++)
     {
-    if (vect[i] == valor)
-    return i;
+    if (vect[posicao] == valor)
+    contar++;
     }
+    printf("%d", contar);
 }
 
-int menor (vector<int> vect, int menor)
+int menor (int menor)
 {
+    vector<int> vect;
      menor = 0;
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if (vect[i] < menor)
         menor = vect[i];
@@ -56,29 +65,32 @@ int menor (vector<int> vect, int menor)
     return menor;
 }
 
-int procurar_menor (vector<int> vect, int menor)
+int procurar_menor_pos (int menor)
 {
+    vector<int> vect;
     menor = 0;
     
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if(vect[i] < menor)
         menor = vect[i];
     }
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if (vect[i] == menor)
         return(i);
     }
+    return 0;
 }
 
-int procurar_menor_apos (vector<int> vect, int menor, int posicao)
+int procurar_menor_pos_apos (int menor, int posicao)
 {
+    vector<int> vect;
     menor = 0;
     posicao = 0;
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if (vect[i] < menor)
         menor = vect[i];
@@ -92,84 +104,91 @@ int procurar_menor_apos (vector<int> vect, int menor, int posicao)
     return posicao;
 }
  
- int procurar_menor_apos_partir (vector<int> vect, int inicio)
+ int procurar_menor_apos_partir ( int inicio)
  {
+     vector<int> vect;
+     int menor = 0;
 
+     for (size_t i = inicio; (int)vect.size(); i++)
+     {
+         if (i == inicio) menor = vect[i];
+         if (menor > vect[i]) menor = vect[i];
+     }
+    return menor;
  }
  
- int procurar_melhor_se (vector<int> vect, int menor, )
+ int procurar_melhor_se (int menor)
  {
+     vector<int> vect;
      menor = 0;
 
-     for (int i = 0; i < vect.size(); i++)
+     for (size_t i = 0; i < vect.size(); i++)
      {
-         if (vect[i] < menor && vect[i] > 0)
-         menor = vect[i];
+         if (i == 0) menor = vect[i];
+         if (menor > vect[i] && vect[i] > 0) menor = vect[i];
      }
      return menor;
  }
  
- float calcular_stress_medio (vector<int> vect, float media, float soma)
+ float calcular_stress_medio (float media, float soma)
  {
+     vector<int> vect;
      media = 0;
      soma = 0;
 
-     for(int i = 0; i < vect.size(); i++)
+     for(size_t i = 0; i < vect.size(); i++)
      {
-         soma += vect[i];
-         media = soma/i;
+         soma += abs(vect[i]);
      } 
+     media = soma / vect.size();
      return media;
  }
  
-int mais_homens_ou_mulheres (vector<int> vect, int homens, int mulheres)
+void mais_homens_ou_mulheres (int homens, int mulheres)
 {
+    vector<int> vect;
     homens = 0;
     mulheres = 0;
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
         if (vect[i] > 0) homens++;
-        else mulheres++;
+        if (vect[i] < 0) mulheres++;
     }
-    if (homens > mulheres) return homens;
-    else return mulheres; 
+    if (homens > mulheres) printf ("%d", homens);
+    else printf("%d", mulheres); 
 }
 
-float qual_metade_eh_mais_estressada (vector<int> vect, float mediaH, float mediaM)
+void qual_metade_eh_mais_estressada (float mediaH, float mediaM)
 {
+    vector<int> vect;
     mediaH = 0;
     mediaM = 0;
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < (vect.size())/2; i++)
     {
-        if (vect[i] < 0) mediaM += vect[i];
-        else mediaH += vect[i];
-        mediaM = mediaM/(i/2);
-        mediaH = mediaH/(i/2);
+        mediaH += abs(vect[i]);
+    }
+    for (size_t i = (vect.size())/2; i < (vect.size())/2; i++)
+    {
+        mediaM += abs(vect[i]);
     }
 
-    if (mediaH > mediaM) cout << "Mais mulheres que homens";
-    else cout << "mais homens que mulheres";
-
-
-
-
-    
+    if (mediaH > mediaM) printf("homens são mais estressados");
+    else printf ("mulheres são mais estressadas");    
 }
 
-float homens_sao_mais_estressados_que_mulheres (vector<int> vect, float mediaM, float mediaH)
+void homens_sao_mais_estressados_que_mulheres (float mediaM, float mediaH)
 {
+    vector<int> vect;
     mediaM = 0;
     mediaH = 0;
 
-    for (int i = 0; i < vect.size(); i++)
+    for (size_t i = 0; i < vect.size(); i++)
     {
-        mediaM = mediaM/(i/2);
-        mediaH = mediaH/(i/2);
-        if (vect[i] < 0) mediaM += vect[i];
-        else mediaH += vect[i];
+        if (vect[i] < 0) mediaM += abs(vect[i]);
+        if (vect[i] > 0) mediaH += abs(vect[i]);
     }
-    if (mediaH > mediaM) return mediaH;
-    else cout << "A média foi menor que o esperado";
+    if (mediaH > mediaM) printf ("Os homens são mais estressados");
+    else printf ("As mulheres são mais estressadas");
 }

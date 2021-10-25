9fe95ba8ddf527e4968809fe3a759982258a0e19
Rafael Oliveira <rafaelfmo@alu.ufc.br>
2021-10-25 00:06:50 -0300

    Lapiseira.cpp

--- Lapiseira.cpp
+++ Lapiseira.cpp
@@ -6,6 +6,25 @@ struct Grafite
     float calibre {0};
     string dureza {""};
     int tamanho {0};
+
+    int Desgaste()
+    {
+        if (this-> dureza == "HB")
+        {
+            return 1;
+        } else if (this-> dureza == "2B")
+        {
+            return 2;
+        } else if (this-> dureza == "4B")
+        {
+            return 4;
+        } else if (this-> dureza == "6B")
+        {
+            return 6;
+        }
+        return 0;
+    }
+    
 };
 
 struct Lapiseira
@@ -13,21 +32,50 @@ struct Lapiseira
     float calibre {0};
     Grafite grafite;
 
-    void Escrever(int tamanho)
+    void Escrever(int folhas)
     {
+        int desgaste = this->grafite.Desgaste();
 
+        for (int i = 0; i != folhas; i++)
+        {
+            if (this->grafite.tamanho - desgaste > 0)
+            {
+                this-> grafite.tamanho -= desgaste;
+            } else if (this-> grafite.tamanho - desgaste <= 0)
+            {
+                cout << "Acabou o grafite" << endl;
+                this-> remover();
+                break;
+            }
+        }
     }
 
-    bool ColocarGrafite(string dureza, float calibre)
+    bool ColocarGrafite(Grafite grafite)
     {
-        if (dureza == "hb" && calibre == 1)
+        if (this->grafite.dureza == "nulll")
+        {
+            if (this->calibre != grafite.calibre)
+            {
+                cout << "Calibre errado" << endl;
+            } else  {
+                this-> grafite = grafite;
+                    }
+            return true;
+        }
+        return false;    
+    }
+
+    Grafite remover()
+    {
+        Grafite aux{this->grafite};
+        this-> grafite = *new Grafite();
+        return aux;
     }
 };
 
 int main()
 {
-    Grafite g = {0.5, "Hb", 1};
-    cout  << "o grafite tem calibre: "<< g.calibre << "\n" << "dureza: " << g.dureza << "\n" << "e tamanho: "<< g.tamanho << endl;
+    
 
     return 0;
 }

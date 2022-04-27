aca3ff9422e0b35069df143be3de2ff2f9c5aa44
Rafael Oliveira <rafaelfmo@alu.ufc.br>
2022-04-27 19:22:38 -0300

    Organizando

--- 14_Cadastro.cpp
+++ 14_Cadastro.cpp
@@ -1,222 +1,216 @@
 #include <iostream>
-#include <vector>
 #include <list>
 #include <map>
+#include <vector>
 #include <memory>
 #include <sstream>
-using namespace std;
+#include <stdexcept>
 
-class Conta
-{
-protected:
+class Account {
+protected: 
     int id;
-    float saldo;
-    string clienteId;
-    string tipo_de_conta {""};
-
+    float balance {0.00};
+    std::string clientId;
+    std::string type{""}; //CC or CP
 public:
-    Conta(int id = 0, string clienteId = "") : id{id}, clienteId{clienteId}{};
+    Account(int id = 0, std::string clientId ="") : id{id}, clientId{clientId} {};
 
-    virtual void Atualizacao_mensal(){}
+    //abstract method
+    virtual void monthlyUpdate() {}
 
-    void Saque(float valor)
-    {
-        this->saldo -= valor;
-    }
-
-    void Deposito(float valor)
-    {
-        this->saldo += valor;
-    }
-
-    void Transferir(shared_ptr<Conta> aux, float valor)
-    {
-        this->saldo -= valor;
-        aux->saldo += valor;
-    }
-
-    int getId()
-    {
-        return this->id;
-    }
-
-    float getSaldo()
-    {
-        return this->saldo;
-    }
-
-    string getClienteId()
-    {
-        return this->clienteId;
-    }            
-
-    string getType()
-    {
-        return this->tipo_de_conta;
-    }    
-
-    friend ostream& operator<< (ostream& os, const Conta& conta)
-    {
-        os << conta.id << ":" << conta.clienteId << ":" << conta.saldo << ":" << conta.tipo_de_conta;
-        return os;
-    }
-};
-
-class VerificandoConta : public Conta
-{
-public:
-    VerificandoConta(int id = 0, string clienteId = "") : Conta{id, clienteId}
-    {
-        tipo_de_conta = "CC";
+    //saque
+    void withdraw(float value) {
+        if (value < balance) {
+            throw std::runtime_error("Saldo insuficiente");
+        } else {
+            balance -= value;
+        }
     };
 
-    void Atualizacao_mensal()
-    {
-        if (this->saldo - 20 < 0)
-        {
-            saldo = 0;
+    //deposito
+    void deposit(float value) {
+        balance += value;
+    };
+
+    //transferencia para outra conta
+    void transfer(Account other, float value) {
+        if(balance < value) {
+            throw std::runtime_error("Saldo insuficiente");
+        } else if(value <= 0) {
+            throw std::runtime_error("Valor invalido");
         }
-        else 
-        {
-            saldo -= 20;
-        }
-    } 
+        other.balance += value;
+        this->balance -= value;
+    };
+
+    friend std::ostream& operator<<(std::ostream& os, const Account& a) {
+        os << a.id << ":" << a.clientId << ":" << a.balance << ":" << a.type;
+        return os;
+    }
+
+    //GETS and SETS
+    int getId() {
+        return id;
+    }
+
+    float getBalance() const {
+        return balance;
+    }
+
+    std::string getClientId() const{
+        return clientId;
+    }
+
+    std::string getType() const{
+        return type;
+    }
 };
 
-class SavingConta : public Conta
-{
+class CheckingAccount : public Account {
 public:
-    SavingConta(int id = 0, string clienteId = "") : Conta{id, clienteId}{}
+    //inicializa conta.type com "CC"
+    CheckingAccount(int id = 0, std::string idClient = "") : Account{id, idClient} {
+        type = "CC";
+    }
 
-    void Atualizaao_mensal()
-    {
-        this->saldo = this->saldo + (saldo * 0.01);
+    //retira 20 do saldo
+    void monthlyUpdate() {
+        if(this->balance - 20 < 0){
+            balance = 0;
+            throw std::runtime_error("Conta bloqueada");
+        } else {
+            balance -= 20;
+        }
     }
 };
 
-class Cliente
-{
-private:
-    list<shared_ptr<Conta>> contas;
-    string clienteId;
-public:    
-    Cliente(string clienteId) : clienteId{clienteId}{}
-
-    void AdicionarConta(shared_ptr<Conta> conta)
-    {
-        this->contas.push_back(conta);
+class SavingsAccount : public Account {
+public:
+    SavingsAccount(int id, std::string idClient) : Account{id, idClient} {
+        type = "CP";
     }
 
-    list<shared_ptr<Conta>> getContas()
-    {
-        return this->contas;
+    //aumenta saldo em 1%
+    void monthlyUpdate() {
+        this->balance = this->balance + (balance * 0.01);
+    }
+};
+
+class Client {
+    std::vector<std::shared_ptr<Account>> accounts;
+    std::string clientId;
+    public:
+    Client (std::string clientId = "", int id = 0): clientId{clientId} {
+        this->addAccount( std::make_shared<CheckingAccount>( id, clientId ));
+        this->addAccount( std::make_shared<SavingsAccount>( (id+1), clientId ));
     }
 
-    string getClienteId()
-    {
-        return this->clienteId;
+    //métodos
+    void addAccount (std::shared_ptr<Account> account) {
+        accounts.push_back(account);
     }
 
-    void setClienteId(string clienteId)
-    {
-        this->clienteId = clienteId;
-    }
-    
-    void setContas(list<shared_ptr<Conta>> contas)
-    {
-        this->contas = contas;
-    }
-
-    friend ostream& operator<<(ostream& os, Cliente& cliente)
-    {
-        for (auto &conta : cliente.contas)
-        {
-            os << *conta << "\n";
+    friend std::ostream& operator<<(std::ostream& os, Client* client) {
+        auto contas = client->getAccounts();
+        os << client->clientId << " [" ;
+        for (int i {0}; i < (int) contas.size(); i++) {
+            if (i != (int) (contas.size() - 1))
+                os << contas[i]->getId() << ", ";
+            else 
+                os << contas[i]->getId() << "]\n";
         }
         return os;
     }
+
+    //gets e sets
+    std::vector<std::shared_ptr<Account>> getAccounts () const {
+        return accounts;
+    }
+
+    std::string getClient() const {
+        return clientId;
+    }
+
+    void setAccount(std::vector<std::shared_ptr<Account>> accounts) {
+        this->accounts = accounts;
+    }
+
+    void setClientId (std::string clientId) {
+        this->clientId = clientId;
+    }
 };
 
-class Banco
-{
+class BankAgency {
 private:
-    map<int, shared_ptr<Conta>> contas;
-    map<string, shared_ptr<Cliente>> clientes;
+    std::map<int, std::shared_ptr<Account>> accounts;
+    std::map<std::string, std::shared_ptr<Client>> clients;
+    
+    Account* getAccount(int id) {
+        return this->accounts[id].get();
+    }
+    void existe (int conta) {
+        if ((int) this->accounts.size() <= conta )
+            throw std::runtime_error("conta nao encontrada");
+    }
+public: 
+    BankAgency() {}
 
-    Conta* getConta(int id)
-    {
-        return this->contas[id].get();
+    void addClient (std::string clientId) {
+        for (auto conta : clients) {
+            if (conta.first == clientId)
+                throw std::runtime_error("ja existe conta");
+        }
+
+        int size { (int) accounts.size()};
+
+        auto client {std::make_shared<Client>(clientId, size)};
+        clients.insert({clientId, client});
+        
+        auto contas = client->getAccounts();
+        for (int i{0}; i < (int) contas.size(); i++) {
+            accounts.insert({size, contas[i]});
+            size++;
+        }   
     }
 
-    void Conta_existe(int conta)
-    {
-        if(this->contas.size() <= conta)
-        {
-            cout << "Erro" << endl;
-        }
-    }
-
-public:
-    Banco(){}
-
-    void AdicionarCliente(string clienteId)
-    {
-        for (auto conta : clientes)
-        {
-            if (conta.first == clienteId)
-            {
-                cout << "Erro" << endl;
-            }
-        }
-    }
-
-    void deposito (int ClienteID, float valor)
-    {
-        auto conta = getConta(ClienteID);
-        if (this->contas.size() <= ClienteID)
-        {
-            cout << "Erro" << endl;
-        }
+    void deposit (int idConta, float value) {
+        auto conta = getAccount(idConta);
+        if ((int) this->accounts.size() <= idConta)
+            throw std::runtime_error("conta nao encontrada");
         else
-        {
-            conta->Deposito(valor);
+            conta->deposit(value);
+    }
+
+    void monthlyUpdate() {
+        for (auto conta : accounts) {
+            conta.second->monthlyUpdate();
         }
+    }
+
+    void transfer (int contaDe, int contaPara, float value) {
+        existe(contaDe);
+        existe(contaPara);
+        auto conta = getAccount(contaDe);
+        
+        conta->transfer( *(this->accounts[contaPara]), value );
+    }
+
+    void withdraw(int idConta, float value) {
+        existe(idConta);
+        auto conta = getAccount(idConta);
+
+        conta->withdraw(value);
     }
     
-    void atualizacao_mensal()
-    {
-        for (auto conta : contas)
-        {
-            conta.second->Atualizacao_mensal();
-        }
-    }
-
-    void transferencia(int conta1, int conta2, float valor)
-    {
-        Conta_existe(conta1);
-        Conta_existe(conta2);
-        auto conta = getConta(conta1);
-
-        conta->Transferir( *(this->contas[conta2]), valor);
-    }
-
-    void Saque(int ClienteID, float valor)
-    {
-        Conta_existe(ClienteID);
-        auto conta = getConta(ClienteID);
-
-        conta->Transferir(valor);
-    }
-
-    friend ostream& operator<<(ostream& os, Banco& banco) {
+    friend std::ostream& operator<<(std::ostream& os, BankAgency& b) {
         int cont = 0;
-        os << "Clientes:\n";
-        for (auto a : banco.clientes) {
+        os << "Clients:\n";
+        for (auto a : b.clients) {
             os << "- " << a.first << "[" << cont << (cont + 1) << "]\n";
             cont += 2;
         }
-        os << "Contas:\n";
-        for (auto c : banco.contas) {
+        os << "Accounts:\n";
+        for (auto c : b.accounts) {
             os << c.second;
         }
         return os;
@@ -224,7 +218,7 @@ public:
 };
 
 int main () {
-    Banco* banco = new Banco;
+    BankAgency* banco = new BankAgency;
     while (true){
         std::string line, cmd;
         std::cin >> cmd;
@@ -237,29 +231,29 @@ int main () {
             else if (cmd == "show")
                 std::cout << banco;
             else if (cmd == "update")
-                banco->atualizacao_mensal();
+                banco->monthlyUpdate();
             else if (cmd == "addCli") {
                 std::string nome;
                 ss >> nome;
-                banco->AdicionarCliente(nome);
+                banco->addClient(nome);
             }
             else if (cmd == "saque") {
                 int conta;
                 float value;
                 ss >> conta >> value;
-                banco->Saque(conta, value);
+                banco->withdraw(conta, value);
             }
             else if (cmd == "deposito") {
                 int conta;
                 float valor;
                 ss >> conta >> valor;
-                banco->deposito(conta, valor);
+                banco->deposit(conta, valor);
             }
             else if (cmd == "transf") {
                 int envio, remetente;
                 float value;
                 ss >> envio >> remetente >> value;
-                banco->transferencia(envio, remetente, value);
+                banco->transfer(envio, remetente, value);
             }
             else
                 std::cout << "fail: comando invalido\n";
